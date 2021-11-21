ca337e01b123ef8a050fdbf8b1163404b84b387c
renge <ddhforme@gmail.com>
2021-11-21 23:54:36 +0800

    init

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,9 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  URL:"http://localhost:8080/",
+  URL: "http://localhost:8080/",
   NODE_ENV: '"development"',
   network: '"development"',
+  HOST: "",
+  PORT: "80"
 })
